93d4a43c6aa34d4633abb7fdaaf571a0cf2c2249
Ieremies Romero <ieremies@gmail.com>
2023-05-26 13:44:52 -0300

    v1

--- script.py
+++ script.py
@@ -1,45 +1,32 @@
-"""
-Objetivo desse script é receber um projeto de pesquisa operacional e
-realizar os experimentos necessários, recuperar estatísticas e gerar
-um relatório com os dados.
-
-Gostaríamos que o script seja capaz de indentificar quais experimentos
-já foram realizados e quais ainda precisam ser realizados, recuperando
-em caso de falhas.
-"""
 import argparse
 import sys
 import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from itertools import product
 from tqdm import tqdm
+from configparser import ConfigParser
+import subprocess
 
-# Varibles that will be set by the parser
+# Varibles that will be set by the parser or config
 repo_path = "proj_path"
 proj_name = "proj_name"
 commit_list = "HEAD"
 command = "command"
 log_path = "./logs"
+single = False
 
-# Command line parser
-# python script.py https://github.com/ieremies/mest-code "[exe:bin] [file:inst]" -c HEAD,HEAD~1
-# TODO python script.py gh:ieremies/mest-code
-parser = argparse.ArgumentParser(
-    prog="RoboLOCo",
-    description="Run experiments and generate reports",
-    epilog="And thanks for all the fish",
-)
-parser.add_argument("repository", help="Git repository path (can be a remote URL).")
-parser.add_argument(
-    "command", help="Command used to run experiments (see readme for syntax)."
-)
-parser.add_argument(
-    "-c",
-    "--commit",
-    help="Must be a list of commits hashs separeted by commas.",
-    nargs=1,
-    default=["HEAD"],
-)
+
+def expand_links(link: str) -> str:
+    l = link.split(":")
+    if len(l) < 0 or l[0].startswith("http"):
+        return link
+    if "h" in l[0]:
+        l[0] = "https://github.com/"
+    elif "l" in l[0]:
+        l[0] = "https://gitlab.com/"
+    elif "c" in l[0]:
+        l[0] = "https://gitlab.ic.unicamp.br/"
+    return "".join(l)
 
 
 def clone_repository():
@@ -54,23 +41,20 @@ def clone_repository():
     proj_name = repo_path.split("/")[-1].split(".")[0]
 
     # if the repository is a remote url
-    if repo_path.startswith("http"):
+    if ":" in repo_path:
         if not os.path.exists(f"./{proj_name}"):
-            os.system(f"git clone {repo_path} {proj_name}")
+            os.system(f"git clone {expand_links(repo_path)} {proj_name}")
         repo_path = proj_name  # new path
-    else:
-        # otherwise, just pull the latest changes
-        os.system(f"git -C {repo_path} pull")
+    os.system(f"git -C {repo_path} pull")
 
 
 def clean_commit_list():
     """
-    Replaces any HEAD reference in the commit list with the actual commit.
+    Resolves any reference to commits like HEAD or HEAD~4
     """
     global commit_list
-    if "HEAD" in commit_list:
-        commit_list.remove("HEAD")
-        commit_list.append(
+    for i in range(len(commit_list)):
+        commit_list[i] = (
             os.popen(f"git -C {repo_path} rev-parse --short HEAD").read().strip()
         )
 
@@ -78,24 +62,27 @@ def clean_commit_list():
 def checkout_and_make(commit):
     global repo_path
     os.system(f"git -C {repo_path} checkout {commit}")
-    os.system(f"make -C {repo_path}")
+    # if Makefile exists inside repo_path
+    if os.path.exists(f"{repo_path}/Makefile"):
+        os.system(f"make -C {repo_path}")
 
 
-def run_experiments(commit, binary, instance):
+def run_experiments(commit, *args):
     global log_path
     # Create log file name
-    log = f"{log_path}/{commit}_{binary.split('/')[-1]}"
-    log += f"_{instance.split('/')[-1]}.log" if instance else ".log"
+
+    log = f"{log_path}/{commit}"
+    for arg in args:
+        log += f"_{arg.split('/')[-1]}"
+    log += ".log"
     # if the log exists, skip
     if os.path.exists(log):
         return
 
-    cmd = f"{binary}"
-    cmd += f" {instance}" if instance else ""
+    cmd = " ".join(args)
     cmd += f" &> {log}"
 
-    # TODO dont use pipe, capture stderr and stdout them save to file
-    # in case of sudden loss of power, we won't think we have the results
+    # TODO pipe
     os.system(cmd)
 
 
@@ -120,26 +107,31 @@ def find_files(path):
 
 
 def run_for_commit(commit):
-    global bin_path, inst_path
+    global bin_path, inst_path, single
 
     params = []
+    print(command)
     for c in command:
-        if c[0] == "[":
-            c = c.split(":")
-            if "[exe" in c[0]:
-                # [:-1] to remove the last char, which is ']'
-                params.append(find_exec(c[-1][:-1]))
-            if "[file" in c[0]:
-                params.append(find_files(c[-1][:-1]))
-            # TODO range param
-        else:
-            params.append([c])
+        if c[0] != "[":
+            params.append(c.split(","))
+            continue
+        c = c[1:-1].split(":")
+        if len(c) < 2:
+            print(c)
+            params.append(c[0].split(","))
+            continue
+        if "exe" in c[0]:
+            params.append(find_exec(c[-1]))
+        elif "file" in c[0]:
+            params.append(find_files(c[-1]))
 
     total_tasks = 1
     for p in params:
         total_tasks *= len(p)
 
-    with ThreadPoolExecutor() as executor:
+    # If single, run in a single thread
+    max_workers = 1 if single else os.cpu_count()
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
         futures = [
             executor.submit(run_experiments, commit, *p) for p in product(*params)
         ]
@@ -150,7 +142,6 @@ def run_for_commit(commit):
 
 
 def run():
-    clone_repository()
     clean_commit_list()
 
     # Ensures logs folder exists
@@ -158,14 +149,69 @@ def run():
         os.mkdir(log_path)
 
     for commit in commit_list:
-        checkout_and_make(commit)
+        # checkout_and_make(commit)
         run_for_commit(commit)
 
 
+# Command line parser
+parser = argparse.ArgumentParser(
+    prog="RoboLOCo",
+    description=(
+        "This is a simple script to run operational research experiments."
+        "It tries to abstract much of the struct of the code it self, allowing to quickly run"
+        "a bunch of combinations of instances and parameters and save its logs"
+    ),
+    epilog="And thanks for all the fish",
+)
+parser.add_argument("repository", help="Git repository path (can be a remote URL).")
+parser.add_argument(
+    "command",
+    help="Command used to run experiments (see readme for syntax).",
+    default=None,
+    nargs="?",
+)
+parser.add_argument(
+    "-c",
+    "--commit",
+    help="Must be a list of commits hashs separeted by commas.",
+    nargs=1,
+    default=None,
+)
+parser.add_argument(
+    "--single", help="Run experiments in a single thread.", default=False
+)
+
 if __name__ == "__main__":
     args = parser.parse_args(sys.argv[1:])
-    print(args)
     repo_path = args.repository
-    command = args.command.split()
-    commit_list = args.commit
+    clone_repository()
+
+    # Read the config file, if present, in the project
+    config = None
+    if os.path.isfile(f"{repo_path}/roboloco.conf"):
+        config = ConfigParser()
+        config.read(f"{repo_path}/roboloco.conf")
+    if os.path.isfile(f"{repo_path}/.roboloco.conf"):
+        config = ConfigParser()
+        config.read(f"{repo_path}/.roboloco.conf")
+
+    if args.command is not None:
+        command = args.command.split()
+    elif config is not None:
+        command = config["DEFAULT"]["command_string"].split()
+
+    if args.commit is not None:
+        commit_list = args.commit[0].split(",")
+    elif config is not None and "commit" in config["DEFAULT"]:
+        commit_list = config["DEFAULT"]["commit"].split(",")
+    else:
+        commit_list = ["HEAD"]
+
+    if args.single is not None:
+        single = bool(args.single)
+    elif config is not None and "single" in config["DEFAULT"]:
+        single = bool(config["DEFAULT"]["single"])
+    else:
+        single = False
+
     run()
